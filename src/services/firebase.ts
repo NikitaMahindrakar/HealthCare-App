9b060fff9c87c8fc129e68010ca5315c9f315116
NikitaMahindrakar <mahindrakarnikita8@gmail.com>
2026-03-27 12:32:41 +0530

    Remove exposed firebase config

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,9 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
+<<<<<<< HEAD
   apiKey: "AIzaSyDjOKxFROJdO8dhGN8_Icf2iyssMTqivgk",
   authDomain: "healthcare-cb61c.firebaseapp.com",
   projectId: "healthcare-cb61c"
+=======
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID
+>>>>>>> 46533d5 (Remove exposed firebase config)
 };
 
 const app = initializeApp(firebaseConfig);
